c574f7424ca0ca5166be1e08c8c97a6bd73def6a
azizaxendrswr <91053502+azizaxendrswr@users.noreply.github.com>
2021-10-11 13:41:18 +0700

    Praktikum 4 Tugas 1

---
+++ AzizaAnantaEndraswara-064002100008-Praktikum-4-Tugas1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 11 13:17:57 2021
+
+@author: Aziza Ananta
+NAMA : AZIZA ANANTA ENDRASWARA
+NIM : 064002100008
+"""
+
+a = int(input("Angka Maksimal : "))
+
+while a > 0:
+    for i in range(a):
+        print(a, end='')
+    print()
+    a -= 1
